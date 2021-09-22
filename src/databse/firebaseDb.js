2b206d871207647e9e469edb09a8b988fbaa52d0
Sonjongkook <63791870+Sonjongkook@users.noreply.github.com>
2021-09-21 22:29:17 -0500

    first_commit

---
+++ src/databse/firebaseDb.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBjY6cmfd5E6N13pSSsuWk7zqfDjolw4KU",
+    authDomain: "waterapplication-d0f3d.firebaseapp.com",
+    databaseURL: "https://waterapplication-d0f3d-default-rtdb.firebaseio.com",
+    projectId: "waterapplication-d0f3d",
+    storageBucket: "waterapplication-d0f3d.appspot.com",
+    messagingSenderId: "557838280362",
+    appId: "1:557838280362:web:0c80ecd6858c9b0145d2d3"
+  };
+
+
+export default firebaseConfig;
---
+++ src/models/Weather.js
@@ -0,0 +1,12 @@
+class Weather {
+    constructor(id, temperature, humidity, time) {
+      this.id = id;
+      this.temperature = temperature;
+      this.humidity = humidity;
+      this.time = time
+    }
+  }
+
+
+
+export default Weather
